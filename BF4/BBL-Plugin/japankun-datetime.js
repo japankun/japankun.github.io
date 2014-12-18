3aa99fc0a314c7d58efbfcc0d36b1c0525292c46
Japan-Kun <scejapankun@yahoo.co.jp>
2014-12-18 15:40:37 +0900

    Create japankun-datetime.js

---
+++ BF4/BBL-Plugin/japankun-datetime.js
@@ -0,0 +1,68 @@
+/**
+* @name DATETIME BBLog Plugin
+* @author japankun
+* @version 0.1 2014/12/18
+* @url https://github.com/japankun/japankun.github.io
+*/
+
+// My Code Works I Don't Know Why
+
+// initialize plugin
+BBLog.handle("add.plugin", {
+	
+	/* Info */
+	id : "jpnkun-datetime",
+	name : "DATETIME BBLog Plugin",
+	build : '20141218',
+	
+	translations : {
+		"en" : {
+			"jpnkun-datetime.enable" : "Enable DATETIME Plugin"
+		}
+	},
+	
+	init : function (instance) {
+	},
+	
+	domchange : function(instance){
+		
+		if (BBLog.cache("mode") == "bf4" && instance.japankunDATETIME.checkPageUrl("^/bf4/(.+?/)?battlereport/show/.+?/.+?/(.+?/)?$")) {
+			if ($("#battlereport-metadata").length) {
+				instance.japankunDATETIME.init(instance);
+			}
+		}
+		
+	},
+	
+	/**
+	* japankunDATETIME
+	*/
+	japankunDATETIME : {
+		
+		init : function(instance){
+			
+			if (!$('#japankun-datetime').length) {
+				
+				var unixTime = $(".base-ago").attr("data-timestamp");
+				var date = new Date(unixTime*1000);
+				
+				
+				$(".base-ago").before('<span id="japankun-datetime" >'+date.toLocaleString()+'</span> - ');
+				
+			}
+			
+		},
+		
+		checkPageUrl : function (pattern) {
+			
+			if (document.location.pathname.match(new RegExp(pattern,'i'))) {
+				return true;
+			}
+			
+			return false;
+			
+		}
+		
+	}
+	
+});
