e1f185bb73ae9c7eacf9110eaf88591e00e78720
Japan-Kun <scejapankun@yahoo.co.jp>
2014-12-19 00:53:28 +0900

    Update japankun-datetime.js

--- BF4/BBL-Plugin/japankun-datetime.js
+++ BF4/BBL-Plugin/japankun-datetime.js
@@ -43,7 +43,7 @@ BBLog.handle("add.plugin", {
 			
 			if (!$('#japankun-datetime').length) {
 				
-				var unixTime = $("#battlereport-metadata-inner .base-ago").attr("data-timestamp");
+				var unixTime = $(".battlereport-metadata-inner .base-ago").attr("data-timestamp");
 				var date = new Date(unixTime*1000);
 				
 				
